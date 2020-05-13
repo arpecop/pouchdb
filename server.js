06c2ebd73524d6c5508600723efbc9f93946c34e
Rudi <rudix.lab@gmail.com>
2020-05-13 15:50:24 +0300

    c

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const cluster = require('cluster');
+const cluster = require('cluster')
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000
 if (cluster.isMaster) {
-  cluster.fork();
-  cluster.on('exit', worker => {
-    console.log('Worker %d died :(', worker.id);
-    cluster.fork();
-  });
+    cluster.fork()
+    cluster.on('exit', (worker) => {
+        console.log('Worker %d died :(', worker.id)
+        cluster.fork()
+    })
 } else {
-  const compression = require('compression');
-  const cors = require('cors');
-  const express = require('express');
-  const app = express();
-  const PouchDB = require('pouchdb');
-  const TempPouchDB = PouchDB.defaults({ prefix: './db/' });
-  app.use(compression());
-  app.use(cors());
-  app.use('/', require('express-pouchdb')(TempPouchDB));
-  app.listen(port, () => {
-    console.log('🔛');
-  });
+    const compression = require('compression')
+    const cors = require('cors')
+    const express = require('express')
+    const app = express()
+    const PouchDB = require('pouchdb')
+    const TempPouchDB = PouchDB.defaults({ prefix: './db/' })
+    app.use(compression())
+    app.use(cors())
+    app.use('/', require('express-pouchdb')(TempPouchDB))
+    app.listen(port, () => {
+        console.log('🔛')
+    })
 }
 
 // dsds
